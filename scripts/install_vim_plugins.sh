5ab20bc7fabbb40ebe5755c560bf20e51b1d8790
Tinge Zhang <tingezhang@gmail.com>
2017-06-18 02:31:06 +0800

    update scripts

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/tingezhang/vtconfig.git
-cd vtconfig
+#git clone https://github.com/tingezhang/vtconfig.git
+cd ~/work/vtconfig
 cp configs/.vimrc configs/.tmux.conf configs/.tmux.status.conf ~/
 mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
