df23f6a43f0aebefbedf0542b0a7f54c62fb1bd9
Tinge Zhang <tingezhang@gmail.com>
2017-06-18 12:31:26 +0800

    update java8 install

--- scripts/install_basic.sh
+++ scripts/install_basic.sh
@@ -12,4 +12,4 @@ touch ~/.ssh/authorized_keys
 
 sudo apt-get update
 sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev  python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
-sudo apt-get install htop cscope ctags ipython3
+sudo apt-get install htop cscope ctags ipython3 software-properties-common
---
+++ scripts/install_java8.sh
@@ -0,0 +1,8 @@
+##############################################
+##refer to
+##https://www.digitalocean.com/community/tutorials/how-to-install-java-with-apt-get-on-ubuntu-16-04
+##############################################
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
